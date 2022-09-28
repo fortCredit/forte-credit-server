67085dbab80102d0b38624b35ff5ac3e0a325d4d
Yhunghabey <yhung.habey1994@gmail.com>
2022-09-28 12:56:28 +0100

    change dnd settings

--- application/services/sms.js
+++ application/services/sms.js
@@ -8,7 +8,7 @@ exports.sendSMS = async (phone, message, correlationID) => {
   }
   const payload = {
     to: phoneNo,
-    from: 'N-Alert',
+    from: 'Myfortvest',
     sms: message,
     type: 'plain',
     channel: 'dnd',
